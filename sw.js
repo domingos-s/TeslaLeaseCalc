923cc82dc14161f91bfdf84059a93beb34f5a8c5
Domingos <domingos.santos.jr@pm.me>
2026-05-23 10:04:16 -0700

    Update icon file extensions from .svg to .png

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 const CACHE = 'lease-mileage-tracker-v1';
-const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.svg', './icons/icon-512.svg'];
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
